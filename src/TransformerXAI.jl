1af9e2b9530fdfd3029286df0041cc7abce76c0e
Charansurya Udaysingh Jhurree <charansurya.udaysingh.jhurree@informatik.hu-berlin.de>
2026-06-08 16:30:50 +0200

    heatmap exp

--- src/TransformerXAI.jl
+++ src/TransformerXAI.jl
@@ -1,7 +1,7 @@
 module TransformerXAI
 
 # Export the public API so tests can see these functions
-export load_llama_model, extract_att_weights_from_layer_llama, AttentionHeatmap
+export load_llama_model, extract_att_weights_from_layer_llama, AttentionHeatmap, visualize_heatmap
 
 # Include source files
 include("loadLlamaModel.jl")
