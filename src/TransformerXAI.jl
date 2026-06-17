60b88e7f0418ca04db074fff8b71ec8748a3ac52
cmsf99 <c.sanchez.fernandez@campus.tu-berlin.de>
2026-06-17 20:36:16 +0200

    Fix package dependencies and attention extraction export

--- src/TransformerXAI.jl
+++ src/TransformerXAI.jl
@@ -3,7 +3,7 @@ module TransformerXAI
 # Export the public API so tests can see these functions
 export 
     load_llama_model, 
-    extract_att_weights_from_layer_llama, 
+    extract_att_weights, 
     AttentionHeatmap, 
     visualize_heatmap,
     generate_attention_heatmap
